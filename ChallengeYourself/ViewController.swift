221da8acfa59ed7aa69529a3695228ed32d6010a
Radu Stefan <radustefan@Radus-MacBook-Pro.local>
2017-02-06 10:31:37 +0200

    Initial Commit

---
+++ ChallengeYourself/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChallengeYourself
+//
+//  Created by Radu Stefan on 06/02/2017.
+//  Copyright © 2017 Endava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
